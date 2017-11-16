9715faeeeca46f2fe02f317cefaa041ece68463d
Jozef Randjak <randjakjozef@gmail.com>
2017-11-16 11:12:11 +0100

    Add entitie classes

--- MeetingLibrary/Class1.cs
+++ MeetingLibrary/Class1.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace MeetingLibrary
 {
-    public class Class1
+    public class MeetingCentreModel
     {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public List<MeetingRoomModel> MeetingRooms { get; set; }
+
+        public MeetingCentreModel()
+        {
+            MeetingRooms = new List<MeetingRoomModel>();
+        }
+    }
+
+    public class MeetingRoomModel
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public int Capacity { get; set; }
+        public bool VideoConference { get; set; }
+        public MeetingCentreModel MeetingCentre { get; set; }
     }
 }
