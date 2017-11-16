fc40ee80ad964b2f0ed0d079e002f81af1495d62
Jozef Randjak <randjakjozef@gmail.com>
2017-11-16 11:13:06 +0100

    Update entitie classes

--- MeetingLibrary/MeetingCentreModel.cs
+++ MeetingLibrary/MeetingCentreModel.cs
@@ -14,14 +14,4 @@ namespace MeetingLibrary
             MeetingRooms = new List<MeetingRoomModel>();
         }
     }
-
-    public class MeetingRoomModel
-    {
-        public string Name { get; set; }
-        public string Code { get; set; }
-        public string Description { get; set; }
-        public int Capacity { get; set; }
-        public bool VideoConference { get; set; }
-        public MeetingCentreModel MeetingCentre { get; set; }
-    }
 }
---
+++ MeetingLibrary/MeetingRoomModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MeetingLibrary
+{
+    public class MeetingRoomModel
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public int Capacity { get; set; }
+        public bool VideoConference { get; set; }
+        public MeetingCentreModel MeetingCentre { get; set; }
+    }
+}
