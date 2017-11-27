29ba3110cf38d18379b6aed9a2f99955630b4b08
Jozef Randjak <randjakjozef@gmail.com>
2017-11-27 17:17:32 +0100

    Add MeetingPlanning entitie

---
+++ MeetingLibrary/MeetingPlanning.cs
@@ -0,0 +1,17 @@
+﻿using Models;
+using System;
+
+namespace MeetingLibrary
+{
+    class MeetingPlanning
+    {
+        public MeetingRoom MeetingRoom { get; set; }
+        public DateTime Date { get; set; }
+        public TimeSpan TimeFrom { get; set; }
+        public int ExpectedPersonsCount { get; set; }
+        public string Customer { get; set; }
+        public bool VideoConference { get; set; }
+        public string Note { get; set; }
+
+    }
+}
